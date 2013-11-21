195b468ec08f2a772c989b0fc4d2a2725035258a
jbaldwin <joshrbaldwin@gmail.com>
2013-11-21 11:31:32 -0700

    updated p10 c sieve to use 0 instead of -1

--- p010/p10.sieve.c
+++ p010/p10.sieve.c
@@ -1,4 +1,3 @@
-#include <malloc.h>
 #include <stdio.h>
 
 #define LIMIT 2000000
@@ -11,23 +10,21 @@ int main(int argc, char* argv[]) {
 	for(i = 0; i < LIMIT; i++) {
 		sieve[i] = i;
 	}
-	sieve[0] = -1; // mark 0 and 1 as non-prime
-	sieve[1] = -1;
+	sieve[0] = 0; // mark 0 and 1 as non-prime
+	sieve[1] = 0;
 
 	for(i = 0; i < LIMIT; i++) {
-		if(sieve[i] != -1) {
+		if(sieve[i] != 0) {
 			int p = sieve[i];
 			for(int j = p + p; j < LIMIT; j += p) {
-				sieve[j] = -1;
+				sieve[j] = 0;
 			}
 		}
 	}
 
-	// all prime numbers are not set to -1.
+	// all non-primes are set to 0
 	for(i = 0; i < LIMIT; i++) {
-		if(sieve[i] != -1) {
-			total += sieve[i];
-		}
+		total += sieve[i];
 	}
 	
 	printf("%llu\n", total);
