cc7efacb9ad6e129d3915e749010cd1e48e5aa41
jbaldwin <joshrbaldwin@gmail.com>
2013-11-21 11:48:29 -0700

    optimized p10 php sieve

--- p010/p10.sieve.php
+++ p010/p10.sieve.php
@@ -3,26 +3,34 @@
 ini_set('memory_limit', '1024M');
 
 $limit = 2000000;
+$climit = floor(sqrt($limit));
 $sieve = array();
+
+// mark all numbers from 2 to climit as potentially prime
 for($i = 0; $i < $limit; $i++) {
-	array_push($sieve, $i);
+	array_push($sieve, true);
+}
+$sieve[0] = false;
+$sieve[1] = false;
+
+// mark all multiples of 2 as non-prime
+for($i = 4; $i < $limit; $i += 2) {
+	$sieve[$i] = false;
 }
 
-$sieve[0] = 0;// set 0 and 1 as non-prime
-$sieve[1] = 0;
-
-for($i = 0; $i < $limit; $i++) {
-	if($sieve[$i] != 0) {
-		$p = $sieve[$i];
-		for($j = $p + $p; $j < $limit; $j += $p) {
-			$sieve[$j] = 0;
+for($i = 3; $i < $climit; $i += 2) {
+	if($sieve[$i] == true) {
+		for($m = $i * $i; $m < $limit; $m += $i * 2) {
+			$sieve[$m] = false;
 		}
 	}
 }
 
 $total = 0;
 for($i = 0; $i < $limit; $i++) {
-	$total += $sieve[$i];
+	if($sieve[$i] == true) {
+		$total += $i;
+	}
 }
 
 print $total . "\n";
