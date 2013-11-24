7edc210dd03231f37ec507910274d763d2cc95e1
jbaldwin <joshrbaldwin@gmail.com>
2013-11-24 14:57:27 -0700

    added p15 php solution

---
+++ p015/p15.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+Starting in the top left corner of a 2x2 grid, and only being able to move to the right and down,
+there are exactly 6 routes to the bottom right corner.
+
+<images not shown>
+
+How many such routes are there through a 20x20 grid?
+**/
+
+/**
+function lattice_paths($x, $y) {
+	if($x == 0 && $y == 0) return 1;
+
+	$total = 0;
+	if($x > 0) $total += lattice_paths($x - 1, $y);
+	if($y > 0) $total += lattice_paths($x, $y - 1);
+	return $total;
+}
+
+print lattice_paths($argv[1], $argv[1]) . "\n";
+**/
+
+$n = $argv[1] * 2;
+$stop = $argv[1];
+$total = 1;
+for($i = 1; $i <= $stop; $i++) {
+	$total = $total * ($n / $i);
+	$n--;	
+}
+print $total . "\n";
+
+?>
+
