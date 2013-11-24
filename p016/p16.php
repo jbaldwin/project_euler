c21ed8deea4f36893ebdbf2f7059fab2626a7664
jbaldwin <joshrbaldwin@gmail.com>
2013-11-24 15:29:55 -0700

    added p16 php solution, reqs gmp

---
+++ p016/p16.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+**/
+
+$two = gmp_init("2");
+$power = 1000;
+$result = gmp_pow($two, $power);
+
+$strval = gmp_strval($result);
+print $strval . "\n";
+$sum = gmp_init("0");
+for($i = 0; $i < strlen($strval); $i++) {
+	$sum = gmp_add($sum, $strval[$i]);
+}
+
+print gmp_strval($sum) . "\n";
+
+?>
+
