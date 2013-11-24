a730108b46f9f1328b9062a3b266f9264a83b1ed
jbaldwin <joshrbaldwin@gmail.com>
2013-11-24 12:27:29 -0700

    change p14 loop

--- p014/p14.php
+++ p014/p14.php
@@ -25,8 +25,7 @@ for($i = 1; $i < 1000000; $i++) {
 	$n = $i;
 	$count = 1;
 	while($n != 1) {
-		if($n % 2 == 0) $n /= 2;
-		else $n = $n * 3 + 1;
+		$n = ($n % 2 == 0) ? $n / 2 : $n * 3 + 1;
 		$count++;
 	}
 	if($count > $max) {
