779a017c04a23b8464de6e2536ddcbb149da783f
jbaldwin <joshrbaldwin@gmail.com>
2013-12-12 18:38:15 -0700

    added p31 c solution

---
+++ p031/p31.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int currency[] = { 100, 50, 20, 10, 5, 2, 1 };
+
+int combos(int i, int start) {
+	if(i == 200) return 1;
+
+	int combinations = 0;
+	for(int j = start; j < 7; j++) {
+		if(i + currency[j] <= 200) {
+			combinations += combos(i + currency[j], j);
+		}
+	}
+	return combinations;
+}
+
+int main(int argc, char* argv[]) {
+
+	int combinations = 1 + combos(0, 0);
+	printf("%i\n", combinations);
+	return 0;
+}
