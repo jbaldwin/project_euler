05f42e0173f4901f03be0c2a1a452b44cb8c2dff
jbaldwin <joshrbaldwin@gmail.com>
2013-12-13 16:21:28 -0700

    cleaned up p39 php solution

--- p039/p39.php
+++ p039/p39.php
@@ -11,42 +11,30 @@ For which value of p <= 1000, is the number of solutions maximised?
 
 // A pythagorean triple is a right triangle where a^2 + b^2 = c^2
 
-$results = array();
+$max_p = 0;
+$max_c = 0;
 
 for($p = 3; $p <= 1000; $p++) {
+	$count = 0;
 	for($a = 1; $a < $p; $a++) {
 	for($b = $a; $b < $p; $b++) {
 		$c = $p - $a - $b;
+
+		if($a + $b <= $c) continue;
 		if($c <= 0) continue;
 
 		if($c * $c == $a * $a + $b * $b) {
-
-			if(!array_key_exists($p, $results)) {
-				$results[$p] = array();
-			}
-
-			array_push($results[$p], array($a, $b, $c));
-
+			$count++;
 		}
 	}
 	}
-}
-
-$p = 0;
-$max = 0;
-foreach($results as $k => $r) {
-	if(count($r) > $max) {
-		$max = count($r);
-		$p = $k;
-	}
-
-	print $k . " " . count($r) . "\n";
-	foreach($r as $triple) {
-		print $triple[0] . " " . $triple[1] . " " . $triple[2] . "\n";
+	if($count > $max_c) {
+		$max_p = $p;
+		$max_c = $count;
 	}
 }
 
-print "\n" . $p . " " . $max . "\n" 
+print "\n" . $max_p . " " . $max_c . "\n" 
 
 ?>
 
