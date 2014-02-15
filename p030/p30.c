2e2be80c55fc31d3f7ca080da1d1bbbb65b1481a
jbaldwin <joshrbaldwin@gmail.com>
2014-02-14 23:31:57 -0500

    working on p30 c solution

---
+++ p030/p30.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char* argv[]) {
+
+	int power = 5;
+	int start = 2;
+	int end = pow(9, 5) * 6;
+
+	int sum = 0;
+
+	for(int i = start; i <= end; i++) {
+		
+	}
+
+	printf("%i", sum);
+
+	return 0;
+}
+
