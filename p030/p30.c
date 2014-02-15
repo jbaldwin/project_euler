a55c4ec9557e417ef0418bdf11e438844c9a9ea2
jbaldwin <joshrbaldwin@gmail.com>
2014-02-15 13:21:38 -0500

    added p30 c solution

--- p030/p30.c
+++ p030/p30.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 
 int main(int argc, char* argv[]) {
@@ -10,7 +11,17 @@ int main(int argc, char* argv[]) {
 	int sum = 0;
 
 	for(int i = start; i <= end; i++) {
-		
+		char s[128];
+		int len = sprintf(s, "%i", i);
+
+		int sum_pow = 0;
+		for(int j = 0; j < len; j++) {
+			sum_pow += pow(s[j] - 48, power);
+		}
+
+		if(sum_pow == i) {
+			sum += i;
+		}
 	}
 
 	printf("%i", sum);
