77c4113354e4e8f6ac237aae5c26022ec846398a
jbaldwin <jbaldwin@spotxchange.com>
2014-12-25 18:02:29 -0700

    cleaned up p1 scala

--- p001/p1.scala
+++ p001/p1.scala
@@ -1,9 +1,10 @@
 object p1 {
 	def main(args: Array[String]) {
-		var range = 0 until 1000;
-		print(range.foldLeft(0)((b, a) => 
-				if(a % 3 == 0 || a % 5 == 0) a + b else b
-			)
+		print(
+			(0 until 1000).foldLeft(0)
+				((b, a) =>
+					if(a % 3 == 0 || a % 5 == 0) a + b else b
+				)
 		)
 	}
 }
