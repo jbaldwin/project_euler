cd2feb8a776bbf72abf3f31a10a1ae6ad5a30108
jbaldwin <jbaldwin@spotxchange.com>
2014-12-27 10:24:05 -0700

    removed malloc from p43 c solution

--- p043/p43.c
+++ p043/p43.c
@@ -4,29 +4,22 @@
 #include <memory.h>
 #include <stdbool.h>
 
-#define MAX_PERMS 1024 * 1024 * 16
-char* permutations[MAX_PERMS][32];
-int i_perm = 0;
+#define MAX_PERMS 3265921 //MAX+1
+#define PERM_LEN  16
 
 /**
  * Concatenates first and second together.
  */
-char* str_concat(char* first, char* second) {
-    char* copy = (char*)malloc(11);
-    memset(copy, '\0', 11);
-
-    memcpy(copy, first, strlen(first));
-    memcpy(&copy[strlen(first)], second, strlen(second));
-    return copy;
+void str_concat(char* dst, char* first, char second) {
+    int len = strlen(first);
+    memcpy(dst, first, strlen(first));
+    dst[len] = second;
 }
 
 /**
- * Shifts all bytes from i+1 by one towards i
+ * Shifts all bytes from i+1 by one towards i into dst.
  */
-char* str_shift(char* str, int i) {
-    char* new_str = (char*)malloc(11);
-    memset(new_str, '\0', 11);
-
+void str_shift(char* dst, char* str, int i) {
     bool i_found = false;
     // shift each byte down wards from i
     for(int j = 0; j < strlen(str) - 1; j++) {
@@ -35,87 +28,81 @@ char* str_shift(char* str, int i) {
         }
 
         if(!i_found) {
-            new_str[j] = str[j];
+            dst[j] = str[j];
         } else {
-            new_str[j] = str[j + 1];
+            dst[j] = str[j + 1];
         }
     }
-    return new_str;
 }
 
 /**
  * Generates all permutations of str into the global permutations array.
  */
-void permute(char* prefix, char* str) {
+void permute(
+    char permutations[MAX_PERMS][PERM_LEN],
+    int* permCount,
+    char* prefix,
+    char* str
+ )
+{
     int len = strlen(str);
     if(len == 0) {
-        if(prefix[0] != '0') {
-            memcpy(permutations[i_perm], prefix, 11);
-            i_perm++;
+        if(prefix[0] != '0') { // ignore all permutations starting with zero
+            memcpy(permutations[*permCount], prefix, 11);
+            (*permCount)++;
         }
     } else {
         for(int i = 0; i < len; i++) {
-            char postfix[2];
-            postfix[0] = str[i];
-            postfix[1] = '\0';
-            char* new_prefix = str_concat(prefix, postfix);
-            char* new_str = str_shift(str, i);
-            permute(new_prefix, new_str);
-            free(new_prefix);
-            free(new_str);
+            char new_prefix[11] = { '\0' };
+            char new_str[11] = { '\0' };
+
+            str_concat(new_prefix, prefix, str[i]);
+            str_shift(new_str, str, i);
+            permute(permutations, permCount, new_prefix, new_str);
         }
     }
 }
 
+// declare global, otherwise stack gets blown, could also malloc
+char permutations[MAX_PERMS][PERM_LEN] = { { 0 } };
+
 int main(int argc, char* argv[]) {
 
-    for(int i = 0; i < MAX_PERMS; i++) {
-        memset(permutations[i], '\0', 32);
-    }
+    int permCount = 0;
 
     char* start = "0123456789";
     char* prefix = "\0\0\0\0\0\0\0\0\0\0";
+
+    // Create all permutations
+    permute(permutations, &permCount, prefix, start);
+
     int divs[] = { 2, 3, 5, 7, 11, 13, 17 };
     unsigned long long sum = 0;
 
-    permute(prefix, start);
-
-    for(int i = 0; i < i_perm; i++) {
-        char* p = (char*)permutations[i];
+    for(int i = 0; i < permCount; i++) {
+        char* p = permutations[i];
+        bool is_divisible = true;
         for(int j = 1; j <= 7; j++) {
-            char temp[4];
-            // sscanf apparently does not like leading zeros (octal?)
-            if(p[j] - 48 == 0)
-            {
-                temp[0] = p[j+1];
-                temp[1] = p[j+2];
-                temp[2] = '\0';
-            }
-            else
-            {
-                temp[0] = p[j];
-                temp[1] = p[j+1];
-                temp[2] = p[j+2];
-                temp[3] = '\0';
-            }
+            char temp[4] = { 0 };
+            memcpy(temp, &p[j], 3);
 
-            unsigned long long d;
-            sscanf(temp, "%llu", &d);
+            unsigned int d;
+            sscanf(temp, "%u", &d);
             if(d % divs[j - 1] != 0) {
-                goto not_divisible;
+                is_divisible = false;
+                break;
             }
         }
 
-        unsigned long long d;
-        sscanf(p, "%llu", &d);
-        sum += d;
-
-not_divisible:
-        // gcc won't compile without an expression after the goto
-        sum += 0;
+        if(is_divisible) {
+            unsigned long long d;
+            sscanf(p, "%llu", &d);
+            sum += d;
+        }
     }
 
     printf("%llu", sum);
 
     return 0;
 }
+
